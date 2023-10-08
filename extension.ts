d254a81d421141d197116b075733e7c4812fc815
urassari <136824813+urassari@users.noreply.github.com>
2023-10-08 22:14:40 +0300

    fixed a mistake.

--- extension.ts
+++ extension.ts
@@ -31,8 +31,8 @@ let cam_y: number = 0
 
     //% block
     export function draw_tilemap(){
-        for (let x = 0; x < tilemap_layout.length; x++) {
-            for (let y = 0; y < 5; y++) {
+        for (let x = 0; x < tilemap_layout[0].length; x++) {
+            for (let y = 0; y < tilemap_layout.length; y++) {
                 kitronik_VIEW128x64.writeImageNoBackOLED(tilemap.get_tilemap(x, y), x*8+cam_x, y*8+cam_y)
             }
         }
