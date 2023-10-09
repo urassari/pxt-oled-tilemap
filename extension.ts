fd6ad6d5962b002b3a84a69290cde2ed38dfcbcf
urassari <136824813+urassari@users.noreply.github.com>
2023-10-10 00:03:26 +0300

    Update extension.ts

--- extension.ts
+++ extension.ts
@@ -6,11 +6,11 @@ let cam_x: number = 0
 let cam_y: number = 0
 
     function replaceCharacter(str: string, index: number, replacement: string) {
-        return (
             str.slice(0, index) +
             replacement +
             str.slice(index + replacement.length)
-        );
+            return str
+
     }
 
 
