46d3514104697cb6b3c8de1a28f967a95ebcce64
urassari <136824813+urassari@users.noreply.github.com>
2023-10-22 19:15:26 +0300

    Update pxt.json, sprites.ts

---
+++ sprites.ts
@@ -0,0 +1,19 @@
+//% color=#d6b10b weight=100 icon=""
+namespace NOT_DONE {
+let sprite_icons: Image[] = []
+let sprite_data: String[][] = [[]]
+
+//% block
+export function add_sprite(image: Image, data: string[]) {
+    sprite_icons.push(image)
+    sprite_data.push(data)
+}
+
+//% block
+export function delete_sprite(indx: number) {
+    sprite_icons.splice(indx, 1)
+    sprite_data.splice(indx, 1)
+}
+
+
+}
