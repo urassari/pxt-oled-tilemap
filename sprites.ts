bb5bdf1908a49d85450322203b6f4f557c391fa3
urassari <136824813+urassari@users.noreply.github.com>
2023-10-22 20:13:58 +0300

    Update sprites.ts

--- sprites.ts
+++ sprites.ts
@@ -15,7 +15,7 @@ export function delete_sprite(indx: number) {
     sprite_data.splice(indx, 1)
 }
 
-//% Block
+//% block
 export function draw_sprites(){
     for (let i = 0; i < sprite_icons.length; i++) {
         kitronik_VIEW128x64.writeImageOLED(sprite_icons[i], parseInt(sprite_data[i][0]), parseInt(sprite_data[i][1]))
